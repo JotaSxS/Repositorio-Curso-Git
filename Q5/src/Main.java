245e06f36dd801e5394bb6a21eaf9809578c2918
Victor Feitosa <victor.rferreira@souunit.com.br>
2025-05-06 20:41:14 -0300

    Questão 5

---
+++ Q5/src/Main.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+
+        PrintWriter ps = new PrintWriter("victor_123456.txt");
+        ps.print("-------- QUESTÃO 5 -------");
+        ps.println();
+        ps.print("alunoAdossantos");
+        ps.println();
+        ps.print("alunoBdossantos");
+        ps.println();
+        ps.print("alunoCdossantos");
+        ps.println();
+        ps.print("alunoDdossantos");
+        ps.println();
+        ps.print("alunoEdossantos");
+        ps.println();
+        ps.close();
+
+    }
+}
